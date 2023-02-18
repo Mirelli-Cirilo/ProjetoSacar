9af7815875b0e5cfc5a26d7a93d5325735fd6152
Maria Mirelli Cirilo <mirellicirilo44@gmail.com>
2023-02-18 20:46:38 -0300

    Ex: notas a serem sacadas.

---
+++ sacar.py
@@ -0,0 +1,28 @@
+valor = int(input('Qual valor será sacado?'))
+total = valor
+ced = 100
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced == 1:
+            print(f'Total de {totalced} cédula de R${ced}')
+        elif totalced > 0:
+            print(f'Total de {totalced} cédulas de R${ced}')
+
+        if ced == 100:
+            ced = 50
+        elif ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        elif ced == 5:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+            
